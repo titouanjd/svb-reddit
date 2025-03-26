e969f1ae3188872af628da64c78e6bcfee0b4fcd
Titouan <81510320+titouanjd@users.noreply.github.com>
2025-03-26 13:56:18 +0100

    add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+# Read the PRAW documentation to get
+# the client ID, secret ID and username:
+# https://praw.readthedocs.io/en/stable/index.html
+
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+USERNAME = "titouanjd"
